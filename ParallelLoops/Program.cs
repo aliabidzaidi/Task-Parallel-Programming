bd3aa153a3619af4cc7d0a18fef945015b138152
Abid Zaidi <abidzaidi7@hotmail.com>
2019-06-21 19:53:58 +0500

    Synchronizing Parallel Loops, Analysis of Parallel Loops, Partitioning Data in Parallel Loops using default strategies

--- ParallelLoops/Program.cs
+++ ParallelLoops/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,7 +18,10 @@ namespace ParallelLoops
             //ParallelInvoke();
             //ParallelLoopBreakStop();
             //CancellingParallelLoop();
-            ParallelLoopUsingTLS();
+            //ParallelLoopUsingTLS();
+            //SynchronizingParallelLoop();
+            ParallelLoopBodyAnalysis();
+            ChunkingPartitionStrategy();
 
             Console.WriteLine("Program has Ended, Press Enter to Exit");
             Console.ReadKey();
@@ -179,7 +183,136 @@ namespace ParallelLoops
         /// </summary>
         static void SynchronizingParallelLoop()
         {
+            Account account = new Account();
+            int itemsPerMonth = 100;
+
+            Account[] transactions = new Account[12 * itemsPerMonth];
+            for (int i = 0; i < 12 * itemsPerMonth; i++)
+            {
+                transactions[i] = new Account() { Amount = 10 };
+            }
+            int[] monthlyBalance = new int[12];
+            for (int currentMonth = 0; currentMonth < 12; currentMonth++)
+            {
+                Parallel.For(currentMonth * itemsPerMonth, (currentMonth + 1) * itemsPerMonth, new ParallelOptions(), () => 0, (index, loopState, sumBalance) =>
+                        {
+                            return sumBalance += transactions[index].Amount;
+                        }, sumBalance => monthlyBalance[currentMonth] += sumBalance);
+
+                if (currentMonth > 0)
+                {
+                    monthlyBalance[currentMonth] += monthlyBalance[currentMonth - 1];
+                }
+            }
+            string[] monthNames = Enum.GetNames(typeof(Months));
+
+            for (int i = 0; i < monthlyBalance.Length; i++)
+            {
+                Console.WriteLine("Sum of Monthly Balance {0} : {1}", monthNames[i], monthlyBalance[i]);
+            }
+        }
+
+        delegate void ProcessValue(int value);
+        static double[] resultData = new double[10000000];
+        static void ComputeSquare(int indexValue)
+        {
+            resultData[indexValue] = Math.Pow(indexValue, 2);
+        }
+
+        /// <summary>
+        /// Listing 5-14
+        /// </summary>
+        static void ParallelLoopBodyAnalysis()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            //Perform simple Parallel Loop
+            Parallel.For(0, resultData.Length, (index) =>
+            {
+                resultData[index] = Math.Pow(index, 2);
+                //Console.WriteLine("Value:{0} -- Square: {1}", index, Math.Pow(index, 2));
+            });
+            watch.Stop();
+            Console.WriteLine("Simple Parallel Loop took {0} Milliseconds", watch.ElapsedMilliseconds);
+
+            //Perform Parallel Loop But make delegate explicit
+            watch = System.Diagnostics.Stopwatch.StartNew();
+            Parallel.For(0, resultData.Length, delegate (int index) { resultData[index] = Math.Pow(index, 2); });
+            watch.Stop();
+            Console.WriteLine("Parallel Loop But make delegate explicit took {0} Milliseconds", watch.ElapsedMilliseconds);
+
+
+            //Perform Loop with Declared Delegate and Action
+            watch = System.Diagnostics.Stopwatch.StartNew();
+            ProcessValue pdel = new ProcessValue(ComputeSquare);
+            Action<int> paction = new Action<int>(pdel);
+            Parallel.For(0, resultData.Length, paction);
+            watch.Stop();
+            Console.WriteLine("Parallel Loop with Declared Delegate and Action took {0} Milliseconds", watch.ElapsedMilliseconds);
 
         }
+
+
+        /// <summary>
+        /// Listing 5-15, 5-16 Default Chunking formula = (n/(3*p)) | {n=no.of.items & p=no.of.processors}
+        /// </summary>
+        static void ChunkingPartitionStrategy()
+        {
+            //By breaking 10,000,000 index values into chunks of 10,000, we reduce the number of times that the
+            //delegate is invoked to 1,000 and improve the ratio of overhead versus index processing
+            var stopWatch = System.Diagnostics.Stopwatch.StartNew();
+            OrderablePartitioner<Tuple<int, int>> partition = Partitioner.Create(0, resultData.Length, 10000);
+
+            Parallel.ForEach(partition, partitionRange =>
+            {
+                for (int i = partitionRange.Item1; i < partitionRange.Item2; i++)
+                {
+                    resultData[i] = Math.Pow(i, 2);
+                }
+            });
+            stopWatch.Stop();
+            Console.WriteLine("Parallel Loop using Chunking Partitions took {0} Milliseconds", stopWatch.ElapsedMilliseconds);
+
+
+            IList<string> sourceData = new List<string>() { "an", "apple", "a", "day", "keeps", "the", "doctor", "away" };
+            string[] adviceData = new string[sourceData.Count];
+            OrderablePartitioner<string> part = Partitioner.Create(sourceData);
+            
+            Parallel.ForEach(part, (string item, ParallelLoopState state, long index) => {
+                if (item == "apple")
+                    item = "apricot";
+                adviceData[index] = item;
+            });
+
+            StringBuilder sB = new StringBuilder();
+            foreach(string s in adviceData)
+            {
+                sB.Append(s + " ");
+            }
+            Console.WriteLine(sB);
+        }
+
     }
+
+    class Account
+    {
+        public int Amount { get; set; }
+    }
+
+    public enum Months
+    {
+        January = 0,
+        February = 1,
+        March = 2,
+        April = 3,
+        May = 4,
+        June = 5,
+        July = 6,
+        August = 7,
+        September = 8,
+        October = 9,
+        November = 10,
+        December = 11
+    }
+
 }
